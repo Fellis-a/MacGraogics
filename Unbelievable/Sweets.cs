34f21967369dfd7bdfb86dcf514b6cb3dd3b2068
Alice <2012alis@mail.ru>
2020-04-06 17:58:19 +0800

    Добавлено новое общее свойство

--- Unbelievable/Sweets.cs
+++ Unbelievable/Sweets.cs
@@ -7,16 +7,17 @@ namespace Unbelievable
 {
     public class Sweets
     {
+        public float Sugar = 0;
         //рандом для дальнейшего использованя
         protected static Random rnd = new Random();
 
 
         public virtual String GetInfo()
         {
-
-            return "Я сладость";//не я
+            var str = string.Format("\nКоличество сахара: {0} г", this.Sugar);
+            return str;
         }
-        //общих своств нет, поэтому больше здесь нечего прписывать
+       
     }
 
 
@@ -39,6 +40,7 @@ namespace Unbelievable
             str += String.Format("\nКоличество плиток: {0}", this.BlocksNumber);
             str += String.Format("\nНачинка: {0}", this.ChocolateFilling ? "с начинкой" : "без начинки");
             str += String.Format("\nТип шоколада: {0}", this.ChocType);
+            str += base.GetInfo();
             return str;
         }
         //задаем параметры при создании объекта
@@ -49,7 +51,8 @@ namespace Unbelievable
             {
                 BlocksNumber = 1 + rnd.Next() % 10,  // количество плиток от 1 до 10
                 ChocType = ChocolatesType[rnd.Next(0, ChocolatesType.Length)],//тип шоколада
-                ChocolateFilling = rnd.Next() % 2 == 0//есть или нет начинки
+                ChocolateFilling = rnd.Next() % 2 == 0,//есть или нет начинки
+                Sugar = 1 + rnd.Next() % 100//кол-во сахара от 1 до 100 грамм
 
             };
         }
@@ -74,6 +77,7 @@ namespace Unbelievable
             var str = "Я фрукт";
             str += String.Format("\nСпелость: {0}", this.Ripeness);
             str += String.Format("\nТип фрукта: {0}", this.Fruit);
+            str += base.GetInfo();
             return str;
         }
 
@@ -84,6 +88,7 @@ namespace Unbelievable
             {
                 Ripeness = rnd.Next() % 101, // спелость от 0 до 100
                 Fruit = FruitsType[rnd.Next(0, FruitsType.Length)],//тип фрукта
+                Sugar = 1 + rnd.Next() % 100//кол-во сахара от 1 до 100 грамм
 
             };
         }
@@ -108,16 +113,18 @@ namespace Unbelievable
             var str = "Я выпечка";
             str += String.Format("\nКалорийность: {0}", this.Calorie);
             str += String.Format("\nТип выпечки: {0}", this.BakeryType);
+            str += base.GetInfo();
             return str;
+
         }
         public static Bakery Generate()
         {
 
             return new Bakery
             {
-                Calorie = 0 + rnd.Next() % 1000,  // калорийность от 0 до 1000
-                BakeryType = BakeryTypes[rnd.Next(0, BakeryTypes.Length)]//тип выпечки
-
+                Calorie = 0 + rnd.Next() % 1000  ,  // калорийность от 0 до 1000
+                BakeryType = BakeryTypes[rnd.Next(0, BakeryTypes.Length)],//тип выпечки
+                Sugar = 1 + rnd.Next() % 100//кол-во сахара от 1 до 100 грамм
             };
         }
     }
